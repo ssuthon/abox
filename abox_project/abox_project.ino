79a9786bcc71b7dfe33615d96bf132ed1c9c669a
Suthon <suthon@gmail.com>
2015-04-27 17:16:31 +0700

    try client write

--- abox_project/abox_project.ino
+++ abox_project/abox_project.ino
@@ -14,6 +14,8 @@
 #include <LiquidCrystal_I2C.h>
 #include "LedControlMS.h"
 #include <SHT2x.h>
+#include <Time.h>
+#include <TimeAlarms.h>
 
 #define I2C_ADDR 0x27 //i2c scanner address
 #define BACKLIGHT_PIN 3 //set up blacklight pin
@@ -35,7 +37,10 @@ byte gateway[] = { 192, 168, 1, 1 };
 // the subnet:
 byte subnet[] = { 255, 255, 255, 0 };
 
-UIPServer server = UIPServer(6000);
+EthernetServer server1(6000);
+EthernetClient activeClient1;
+EthernetServer server2(6001);
+EthernetClient activeClient2;
 
 LiquidCrystal_I2C lcd(I2C_ADDR,2,1,0,4,5,6,7);
 
@@ -48,7 +53,9 @@ void setup() {
   //sensor part
   Wire.begin();
   // start listening for clients
-  server.begin();
+  server1.begin();
+  server2.begin();
+  
   Serial.println("Server Ready");
   lcd.begin (20,4);
   // Switch on the backlight
@@ -63,6 +70,103 @@ void setup() {
   lc.setIntensity(0,2);
   lc.clearDisplay(0);
   
+  Alarm.timerRepeat(3, displayBoxInfo);
+}
+
+char cmd_buf[256];
+char response[128];
+char response_body[120];
+int cmd_index = 0; 
+byte serial2_forward = 1;
+long rfid_stamp = 0;
+long serial2_stamp = 0;
+float current_rh = 0;
+float current_tp = 0;
+
+void loop() {
+  EthernetClient client1 = server1.available();
+  EthernetClient client2 = server2.available();
+  
+  if(client1 && activeClient1 != client1){
+    activeClient1.stop();
+    activeClient1 = client1;
+  }
+    
+  if(client2 && activeClient2 != client2){
+    activeClient2.stop();
+    activeClient2 = client2;
+  }
+  
+  int done = 0;
+  while(activeClient1.available() > 0 && !done) {
+    done = processCommand(activeClient1.read());
+  }
+  if(activeClient1 && !activeClient1.connected()){
+    activeClient1.stop();
+  }
+  if(activeClient2 && !activeClient2.connected()){
+    activeClient2.stop();
+  }  
+  
+  Alarm.delay(0);
+}
+
+int processCommand(char tmp){
+  int succeeded = 0;
+  
+   if(tmp != '\n'){
+      cmd_buf[cmd_index++] = tmp;
+      return 0;
+    }else{
+      cmd_buf[cmd_index] = '\0';
+      cmd_index = 0;      
+    }
+    
+    char *cmd_method;
+    char *cmd_spec;
+    
+    cmd_method = trimwhitespace(cmd_buf);
+    if(cmd_method[2] != '_'){
+      Serial.println("invalid command");
+      return 1;
+    }
+    
+    cmd_method[2] = '\0';
+    cmd_spec = cmd_method + 3;
+    response_body[0] = '\0';
+    
+    switch(cmd_method[0]){
+       case 'T':  //text lcd
+         displayTextLcd(cmd_method[1] - '0', cmd_spec);
+         succeeded = 1;
+         break;
+      
+       case 'M':  //matrix led 
+         displayMatrixLed(cmd_spec);
+         succeeded = 1;
+         break;
+         
+       case 'S':  //sensor
+         readSensors(response_body);
+         succeeded = 1;
+         break;
+         
+       case 'C':  //clock
+         setCurrentTime(cmd_spec);
+         succeeded = 1;
+         break;
+         
+       case 'R':  //rs232
+         serial2_forward = (cmd_method[1] == '0') ? 0 : 1;
+         succeeded = 1;
+         break;
+    }
+    if(succeeded && activeClient1.connected()){
+      sprintf(response, "OK_%s_%s\r\n", cmd_method, response_body);
+      activeClient1.print(response);    
+      activeClient1.flush();
+    } 
+    return 1;
 }
 
 void prepareSpec(char *spec, int limit, char dv){
@@ -72,6 +176,13 @@ void prepareSpec(char *spec, int limit, char dv){
     spec[i] = dv;
   }
   spec[limit] = '\0';
+}
+
+void setCurrentTime(char *spec){
+  spec[2] = '\0';
+  spec[5] = '\0';
+  spec[8] = '\0';
+  setTime(atoi(spec), atoi(spec+3), atoi(spec+6), 0, 0, 0);
 }
 
 void displayTextLcd(int line, char *spec){  
@@ -100,76 +211,12 @@ void displayMatrixLed(char *spec){
 void readSensors(char *body){
   float rh = SHT2x.GetHumidity();
   float tc = SHT2x.GetTemperature();
-  Serial.print("Humidity(%RH): ");
+  /*Serial.print("Humidity(%RH): ");
   Serial.println(rh);
   Serial.print("Temperature(C): ");
-  Serial.println(tc);
+  Serial.println(tc);*/
   
-  sprintf(body, "RH=%.2f,TC=%.2f", rh, tc);
-}
-
-char cmd_buf[256];
-char response[128];
-char response_body[120];
-char tmp;
-int cmd_index = 0;
-char *cmd_method;
-char *cmd_spec;
-int line;
-int succeeded;  
-int serial2_forward = 1;
-
-void loop() {
-  // if an incoming client connects, there will be bytes available to read:
-  UIPClient client = server.available();
-  if(client) {
-    tmp = client.read();
-    if(tmp != '\n'){
-      cmd_buf[cmd_index++] = tmp;
-      return;
-    }else{
-      cmd_buf[cmd_index] = '\0';
-      cmd_index = 0;      
-    }
-    
-    cmd_method = trimwhitespace(cmd_buf);
-    if(cmd_method[2] != '_'){
-      Serial.println("invalid command");
-      return;
-    }
-    
-    cmd_method[2] = '\0';
-    cmd_spec = cmd_method + 3;
-    response_body[0] = '\0';
-    
-    switch(cmd_method[0]){
-       case 'T':  //text lcd
-         line = cmd_method[1] - '0';
-         displayTextLcd(line, cmd_spec);
-         succeeded = 1;
-         break;
-      
-       case 'M':  //matrix led 
-         displayMatrixLed(cmd_spec);
-         succeeded = 1;
-         break;
-         
-       case 'S':  //sensor
-         //readSensors(response_body);
-         succeeded = 1;
-         break;
-         
-       case 'R':  //rs232
-         serial2_forward = (cmd_method[1] == '0') ? 0 : 1;
-         succeeded = 1;
-         break;
-      
-    }
-    if(succeeded){
-      sprintf(response, "OK_%s_%s\r\n", cmd_method, response_body);
-      server.print(response);    
-    }
-  }   
+  sprintf(body, "RH=%d,TC=%d", (int)(rh *100), (int)(tc*100));
 }
 
 char code[12];
@@ -207,13 +254,17 @@ void process_code() {
   if(test == checksum){ //valid tag code
     code[10] = '\0';
     Serial.println(code);
-    server.print("RF_");
-    server.print(code);
-    server.print("\r\n");
+    rfid_stamp = millis();
+    if(activeClient1.connected()){
+      activeClient1.print("RF_");
+      activeClient1.print(code);
+      activeClient1.print("\r\n");
+      activeClient1.flush();
+    }
   }
 }
 
-char rs_buf[128];
+byte rs_buf[128];
 int rsi = 0;
 void serialEvent2() { 
   int ok = 0;
@@ -226,11 +277,14 @@ void serialEvent2() {
    }
 
    if(rsi > 0 && ok){
-     rs_buf[rsi] = '\0';
-     if(serial2_forward){
-       server.print("SR_");
-       server.print(trimwhitespace(rs_buf));
-       server.print("\r\n");
+     //rs_buf[rsi] = '\0';
+     serial2_stamp = millis();
+     if(activeClient2.connected() && serial2_forward){
+       //server.print("SR_");
+       //server.print(trimwhitespace(rs_buf));
+       //server.print("\r\n");
+       activeClient2.write(rs_buf, rsi);
+       activeClient2.flush();
      }
      rsi = 0;
    }else if(rsi > 127){
@@ -238,6 +292,22 @@ void serialEvent2() {
    }  
 }
 
+void displayBoxInfo(){
+    char *str = (char*)malloc(21);
+    long now = millis();
+    
+    sprintf(str, "%d%03d %02d%02d %c%c%c  %02d:%02d", ip[2], ip[3], 
+      current_rh > 99 ? 99 : (int)(current_rh),
+      current_tp > 99 ? 99 : (int)(current_tp),
+      (now -serial2_stamp) < 2000 ? 'S' : ' ',
+      activeClient2.connected() ? 'N' : ' ',  
+      (now - rfid_stamp) < 4000 ? 'R' : ' ',  
+      hour(), minute());
+    displayTextLcd(4, str);
+    free(str);
+}
+
+//------utilities functions-------------
 byte hexstr2b(char *s){
   return (byte)((hexchar2b(s[0]) * 16) + hexchar2b(s[1]));
 }
